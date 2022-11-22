32f2712770f6a99daab0d3b9649f4226d499c61e
guilhermepolesi <guilherme-polesi@hotmail.com>
2022-11-22 20:48:02 -0300

    Project created

---
+++ src/main/java/com/educandoweb/Webservicesspringbootjpa/WebServicesSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.Webservicesspringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesSpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/Webservicesspringbootjpa/WebServicesSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.Webservicesspringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
